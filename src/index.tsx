45686b9ffdfe1241bd3d0ec8a1a369a4aac94e4e
Pavel Gordienko <guzmo@yandex.ru>
2024-05-01 19:55:40 +0300

    chore: add eslint config

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ document.body.appendChild(root);
 ReactDOM.render(<App />, root);
 
 if (process.env.NODE_ENV === 'production') {
-    console.log('Running in production mode', process.env.NODE_ENV);
-  } else {
-    console.log('Running in development mode', process.env.NODE_ENV);
-  }
+  console.log('Running in production mode', process.env.NODE_ENV);
+} else {
+  console.log('Running in development mode', process.env.NODE_ENV);
+}
