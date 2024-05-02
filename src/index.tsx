30e95139f697db0b9516596d8b253be90ceccd14
Pavel Gordienko <guzmo@yandex.ru>
2024-05-02 14:21:16 +0300

    chore: example change

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = document.createElement('div');
 root.id = 'root';
 document.body.appendChild(root);
 
-ReactDOM.render(<App />, root);
+ReactDOM.createRoot(root).render(<App />);
 
 if (process.env.NODE_ENV === 'production') {
   console.log('Running in production mode', process.env.NODE_ENV);
 } else {
   console.log('Running in development mode', process.env.NODE_ENV);
-}
+} 
