84f4dfe9b2284c0c262c8c152793d6e02e15664d
Ranika23 <bveronikal@gmail.com>
2024-05-04 08:28:30 +0300

    refactor: change example test case

--- src/__tests__/tests.tsx
+++ src/__tests__/tests.tsx
@@ -1,5 +1,5 @@
-const summ = require('../test-example');
+import sumNumbers from '../test-example';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(summ(1, 2)).toBe(3);
+  expect(sumNumbers(1, 2)).toBe(3);
 });
