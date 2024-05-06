63a843766a2ac3d98253d37381d28ab18223d579
VlaRu <dn210690nvs@gmail.com>
2024-05-06 10:43:04 +0200

    chore: install bootstrap and add in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './styles.css';
+import { Button } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>eCommerce</h1>
       <h2>Project Test</h2>
+      <Button>click</Button>
     </div>
   );
 };
