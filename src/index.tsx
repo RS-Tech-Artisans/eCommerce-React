2c2d18ac3292a570ab38481279f83baa9b6c0576
Pavel Gordienko <guzmo@yandex.ru>
2024-05-06 19:46:16 +0300

    chore: RSS-ECOMM-1_12 (#11)
    * chore: add husky
    
    * chore: add husky hooks
    
    * fix: eslint check
    
    * fix: test check

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,3 @@ root.id = 'root';
 document.body.appendChild(root);
 
 ReactDOM.createRoot(root).render(<App />);
-
-if (process.env.NODE_ENV === 'production') {
-  console.log('Running in production mode', process.env.NODE_ENV);
-} else {
-  console.log('Running in development mode', process.env.NODE_ENV);
-}
