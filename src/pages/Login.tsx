eaaa54efb7a9e2ef389aa4373ad90ebbbc9331f3
VlaRu <dn210690nvs@gmail.com>
2024-05-07 17:17:30 +0200

    refactor: correct styles, rename link

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,3 @@
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
 export default function Login() {
-  return (
-    <>
-      <h1>Login page</h1>
-      <Link to="/">
-        <Button>Login</Button>
-      </Link>
-      <Link to="/registration">
-        <Button>Sig-in</Button>
-      </Link>
-    </>
-  );
+  return <h1>Login page</h1>;
 }
