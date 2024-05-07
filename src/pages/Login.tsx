4d95f73b34dd455dff9efba7999f203cba05af50
VlaRu <dn210690nvs@gmail.com>
2024-05-07 14:02:55 +0200

    feat: add buttons with links to login and registration

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,20 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
 export default function Login() {
-  return <h1>Login page</h1>;
+  return (
+    <>
+      <h1>Login page</h1>
+      <Link to="/">
+        <Button>Login</Button>
+      </Link>
+      <Link to="/registration">
+        <Button>Sig-in</Button>
+      </Link>
+    </>
+  );
 }
+
+
+
