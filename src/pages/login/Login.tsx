eb17a1ffc58eed6f959e8a0ef9564496d5afe4bc
Ranika23 <bveronikal@gmail.com>
2024-05-09 08:15:05 +0300

    feat: add login-form

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,28 @@
+import { MdEmail } from 'react-icons/md';
+import { FaLock } from 'react-icons/fa';
+
 export default function Login() {
-  return <h1>Login page</h1>;
+  return (
+    <>
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="email" placeholder="E-mail" required />
+          <MdEmail />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+          <FaLock />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+          <div>
+            <p>
+              Don&apos;t have an account? <a href="#">Register</a>
+            </p>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
