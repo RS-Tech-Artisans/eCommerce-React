ca2ec5f6d069f453aa5c956d4868a05303fe6d57
VlaRu <dn210690nvs@gmail.com>
2024-05-14 22:19:31 +0200

    refactor: change LocalStorage to SessionStorage

--- src/utils/Login.ts
+++ src/utils/Login.ts
@@ -20,7 +20,7 @@ export const useLogin = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const loggedInStatus = localStorage.getItem('isLoggedIn') === 'true';
+    const loggedInStatus = sessionStorage.getItem('isLoggedIn') === 'true';
     setIsLoggedIn(loggedInStatus);
   }, []);
 
@@ -64,7 +64,7 @@ export const useLogin = () => {
       setLoginResult(result);
       setError(null);
       setIsLoggedIn(true);
-      localStorage.setItem('isLoggedIn', 'true');
+      sessionStorage.setItem('isLoggedIn', 'true');
       navigate('/main');
     } catch (caughtError) {
       console.log(caughtError);
@@ -76,6 +76,6 @@ export const useLogin = () => {
     loginResult,
     error,
     handleLogin,
-    isLoggedIn
+    isLoggedIn,
   };
 };
