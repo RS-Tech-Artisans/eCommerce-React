a954c507e077ca2df6a1f89eb8c838f9e712752b
Ranika23 <bveronikal@gmail.com>
2024-05-15 11:19:25 +0300

    feat: add validation and error message for email input and password input

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,20 +1,52 @@
+import { useState } from 'react';
+import BlurHandler from '../utils/validation/BlurHundler';
+import EmailValidation from '../utils/validation/EmailValidation';
+import PasswordValidation from '../utils/validation/PasswordValidation';
 import { MdEmail } from 'react-icons/md';
 import { FaLock, FaUserCircle } from 'react-icons/fa';
 import './Registration.css';
 
 export default function Registration() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailErr, setEmailErr] = useState('Please fill out this field');
+  const [passwordErr, setPasswordErr] = useState('Please fill out this field');
+  const [emailFill, setEmailFill] = useState(false);
+  const [passwordFill, setPasswordFill] = useState(false);
+
   return (
     <>
       <form className="registration-form" action="">
         <h1>Registration</h1>
         <div className="registration-form_input-box">
-          <input type="email" placeholder="E-mail" required />
+          <input
+            onInput={(e) => EmailValidation(e, email, setEmail, setEmailErr)}
+            onBlur={(e) => BlurHandler(e, setEmailFill, setPasswordFill)}
+            name="email"
+            type="text"
+            placeholder="E-mail"
+            autoComplete="off"
+          />
           <MdEmail />
         </div>
+        {emailFill && emailErr && (
+          <div style={{ color: 'red' }}>{emailErr}</div>
+        )}
         <div className="registration-form_input-box">
-          <input type="password" placeholder="Password" required />
+          <input
+            onInput={(e) =>
+              PasswordValidation(e, password, setPassword, setPasswordErr)
+            }
+            onBlur={(e) => BlurHandler(e, setEmailFill, setPasswordFill)}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
           <FaLock />
         </div>
+        {passwordFill && passwordErr && (
+          <div style={{ color: 'red' }}>{passwordErr}</div>
+        )}
         <div className="registration-form_input-box">
           <input type="text" placeholder="First Name" required />
           <FaUserCircle />
