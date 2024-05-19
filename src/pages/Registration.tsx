ff5e96d8c4aa3f2439731ccbba0f42dc850f5920
Pavel Gordienko <guzmo@yandex.ru>
2024-05-19 03:08:51 +0300

    feat: add example client registration

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,3 +1,42 @@
+import { useState } from 'react';
+import { useRegistration } from '../utils/Registration';
+
 export default function Registration() {
-  return <h1>Registration page</h1>;
+  const { loginResult, error, handleRegistration } = useRegistration();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    await handleRegistration(email, password);
+  };
+
+  return (
+    <div>
+      <h1>Registration page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+      {loginResult && <p>Registration successful!</p>}
+      {error && <p>Error: {error.message}</p>}
+    </div>
+  );
 }
---
+++ src/utils/Registration.ts
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import {
+  ClientResponse,
+  CustomerDraft,
+  CustomerSignInResult,
+} from '@commercetools/platform-sdk';
+import { apiRoot } from './getProjectInfo';
+
+interface MyApiError {
+  message: string;
+}
+
+export const useRegistration = () => {
+  const [loginResult] =
+    useState<ClientResponse<CustomerDraft> | null>(null);
+  const [error, setError] = useState<MyApiError | null>(null);
+
+  const handleRegistration = async (email: string, password: string) => {
+    console.log('email ', email);
+    console.log('password ', password);
+
+    try {
+      const result: ClientResponse<CustomerSignInResult> = await apiRoot
+        .me()
+        .signup()
+        .post({
+          body: {
+            email: 'jenner@example.com',
+            password: 'password',
+            firstName: 'Jenniferr',
+            lastName: 'Robinsonn',
+            dateOfBirth: '2013-12-22',
+            addresses: [
+              {
+                country: 'US',
+                city: 'n',
+                streetName: 'd2g',
+                postalCode: '10001-1234',
+              },
+            ],
+          },
+        })
+        .execute();
+      //setLoginResult(result);
+      console.log(result);
+      setError(null);
+    } catch (caughtError) {
+      console.log(caughtError);
+      setError(caughtError as MyApiError);
+    }
+  };
+
+  return {
+    loginResult,
+    error,
+    handleRegistration,
+  };
+};
