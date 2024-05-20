e20e8251e131e7c5e23d40546d3a7c0be1925dd3
Ranika23 <bveronikal@gmail.com>
2024-05-20 15:19:27 +0300

    feat: add styles for pages

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -13,6 +13,7 @@ import TogglePassInput from '../utils/validation/TogglePassInput';
 import { MdEmail } from 'react-icons/md';
 import { FaLock, FaUnlock, FaUserCircle } from 'react-icons/fa';
 import './Registration.css';
+import './Pages.css';
 
 type RestBlurHandlerRegistrProps = [
   setNameUserFill: React.Dispatch<React.SetStateAction<boolean>>,
