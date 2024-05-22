6c1758ce1049ab28a8df3a8ac2e76e4009e5618a
VlaRu <dn210690nvs@gmail.com>
2024-05-22 22:55:42 +0200

    feat: implement user profile icon with router, delete unnessesary commented code

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import './NavBar.css';
 import LogoutButton from '../components/LogoutButton';
 import { useSession } from '../utils/SessionContext';
+import { CgProfile } from "react-icons/cg";
 
 function NavBar() {
   const { token } = useSession();
@@ -12,6 +13,7 @@ function NavBar() {
         <Link to="/main"> Main </Link>
         <Link to="/login"> Login </Link>
         <Link to="/register"> Registration </Link>
+        {token && <Link to="/profile"><CgProfile /></Link>}
         {token && <LogoutButton />}
       </Navbar>
     </div>
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,13 +1,12 @@
 import { Button } from 'react-bootstrap';
 import { useLogin } from '../utils/Login';
 
+
 export default function LogoutButton() {
-  //const { isLoggedIn, handleLogout } = useLogin();
   const { handleLogout } = useLogin();
 
   return (
     <div>
-      {/* {isLoggedIn ? <Button onClick={handleLogout}>Logout</Button> : <></>} */}
       <Button onClick={handleLogout}>Logout</Button>
     </div>
   );
