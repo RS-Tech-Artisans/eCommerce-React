50c1a84c46d88eb8f6e762532179b5b081e65b7e
Pavel Gordienko <guzmo@yandex.ru>
2024-05-28 21:49:33 +0300

    feat: add useMemo for search result

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ProductCard from './ProductCard';
-import { ProductInfo } from '../utils/Catalog';
-import './ProductGrid.css'; // Import the CSS file
-import { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import ProductCard, { ProductInfo } from './ProductCard';
+import './ProductGrid.css';
 import { Form, InputGroup } from 'react-bootstrap';
 import { BsSearch } from 'react-icons/bs';
+
 interface ProductGridProps {
   products: ProductInfo[];
 }
@@ -12,28 +11,30 @@ interface ProductGridProps {
 const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
   const [search, setSearch] = useState('');
 
-  const filteredProducts = search
-    ? products.filter((item) =>
+  const filteredProducts = useMemo(() => {
+    return search
+      ? products.filter((item) =>
         item.name.toLowerCase().includes(search.toLowerCase())
       )
-    : products;
+      : products;
+  }, [search, products]);
 
   return (
     <div>
-      <Form action="">
-        <InputGroup style={{ width: '30%' }}>
-          <span className="search-icon">
+      <Form>
+        <InputGroup className="search-input-group">
+          <InputGroup.Text>
             <BsSearch />
-          </span>
+          </InputGroup.Text>
           <Form.Control
             value={search}
             placeholder="Search"
             onChange={(e) => setSearch(e.target.value)}
-          ></Form.Control>
+          />
         </InputGroup>
       </Form>
       <div className="product-grid">
-        {products.length > 0 ? (
+        {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
             <ProductCard
               key={product.name}
