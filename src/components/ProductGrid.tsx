1e07e098076c7b693e0c0db4cda22236cb919340
Vladyslava <90470263+VlaRu@users.noreply.github.com>
2024-05-28 18:05:39 +0200

    feat: RSS-ECOMM-3_05 (#47)
    * feat: implement search product form
    
    * fix: fix eslint changed
    
    * fix: delet unnecessary condition filteredProducts

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -2,27 +2,51 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import { ProductInfo } from '../utils/Catalog';
 import './ProductGrid.css'; // Import the CSS file
-
+import { useState } from 'react';
+import { Form, InputGroup } from 'react-bootstrap';
+import { BsSearch } from 'react-icons/bs';
 interface ProductGridProps {
   products: ProductInfo[];
 }
 
 const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
+  const [search, setSearch] = useState('');
+
+  const filteredProducts = search
+    ? products.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      )
+    : products;
+
   return (
-    <div className="product-grid">
-      {products.length > 0 ? (
-        products.map((product) => (
-          <ProductCard
-            key={product.name}
-            name={product.name}
-            imageUrl={product.imageUrl}
-            description={product.description}
-            price={product.price}
-          />
-        ))
-      ) : (
-        <p>No products found</p>
-      )}
+    <div>
+      <Form action="">
+        <InputGroup style={{ width: '30%' }}>
+          <span className="search-icon">
+            <BsSearch />
+          </span>
+          <Form.Control
+            value={search}
+            placeholder="Search"
+            onChange={(e) => setSearch(e.target.value)}
+          ></Form.Control>
+        </InputGroup>
+      </Form>
+      <div className="product-grid">
+        {products.length > 0 ? (
+          filteredProducts.map((product) => (
+            <ProductCard
+              key={product.name}
+              name={product.name}
+              imageUrl={product.imageUrl}
+              description={product.description}
+              price={product.price}
+            />
+          ))
+        ) : (
+          <p>No products found</p>
+        )}
+      </div>
     </div>
   );
 };
