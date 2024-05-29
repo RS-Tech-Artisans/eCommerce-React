d9469785f3ff110bbdcb39b081dbb6fb5f9b171d
VlaRu <dn210690nvs@gmail.com>
2024-05-29 18:37:17 +0200

    refactor: split code for two files, change struction of code request

---
+++ src/components/GetSortingRequest.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { apiRoot } from '../utils/getProjectInfo';
+import { ProductProjection } from '@commercetools/platform-sdk';
+
+export const GetSorting = () => {
+  const [sortAttribute, setSortAttribute] = useState('');
+  const [responseData, setResponseData] = useState<ProductProjection[]>([]);
+  console.log(`response is ${responseData}`);
+
+
+  const getSortProduct = async () => {
+    return apiRoot
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          sort: [sortAttribute],
+        },
+      })
+      .execute()
+      .then((response) => response.body);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (sortAttribute) {
+        const response = await getSortProduct();
+        setResponseData(response.results);
+        console.log(`this ${response.results}`);
+      }
+    };
+    fetchData();
+  }, [sortAttribute]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortAttribute(e.target.value);
+  };
+
+  return { sortAttribute, handleChange, getSortProduct };
+};
