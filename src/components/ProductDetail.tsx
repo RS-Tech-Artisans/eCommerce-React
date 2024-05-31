a9bff32dd9f0d7fa2800cf245e93f9d2d7c04171
VlaRu <dn210690nvs@gmail.com>
2024-05-31 23:29:09 +0200

    feat: realize fetching product details

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,9 +1,54 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { getProductDetailById } from '../utils/api/getProductsDetail';
+import { ProductCardProps } from '../utils/Interfaces';
+import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { mapProducts } from '../utils/productMapper';
+import { MdArrowBackIos } from 'react-icons/md';
 
 const ProductDetail: React.FC = () => {
+  const [product, setProduct] = useState<ProductCardProps | null>(null);
   const { id } = useParams<{ id: string }>();
 
-  return <div>Product Page for ID: {id}</div>;
+  useEffect(() => {
+    const getProductData = async () => {
+      if (id) {
+        try {
+          const fetchProducts = await getProductDetailById(id);
+          console.log('Fetched dataaaaaaaaa:', fetchProducts);
+          const productDetail = mapProducts([fetchProducts]);
+          setProduct(productDetail[0]);
+        } catch (error) {
+          console.error('Error fetching product details:', error);
+        }
+      }
+    };
+    getProductData();
+  }, [id]);
+
+  return (
+    <>
+      <div>Product Page for ID: {id} </div>
+      <Container>
+        <Link to={`/catalog`} style={{ fontSize: '20px' }}>
+          <MdArrowBackIos /> To Catalog
+        </Link>
+        {product ? (
+          <Container className="d-flex p-2 flex-row">
+            <div style={{ marginRight: '20px' }}>
+              <img src={product.imageUrl} alt="" width={400} height={400} />
+            </div>
+            <div>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+            </div>
+          </Container>
+        ) : (
+          <p>Loading product details...</p>
+        )}
+      </Container>
+    </>
+  );
 };
 
 export default ProductDetail;
---
+++ src/utils/api/getProductsDetail.ts
@@ -0,0 +1,11 @@
+import { apiRoot } from './getProjectInfo';
+
+export const getProductDetailById = (ID: string) => {
+  return apiRoot
+    .productProjections()
+    .withId({ ID })
+    .get()
+    .execute()
+    .then((response) => response.body);
+  /* .then((body)=> body.results) */
+};
