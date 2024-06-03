a0a8db608f63f18a728bf3ff9227d048fea4a1b4
Pavel Gordienko <guzmo@yandex.ru>
2024-06-03 15:22:35 +0300

    fix: eslint

--- src/utils/api/getCategories.ts
+++ src/utils/api/getCategories.ts
@@ -2,19 +2,22 @@ import { CategoryPagedQueryResponse } from '@commercetools/platform-sdk';
 import { apiRoot } from './getProjectInfo';
 
 const DEFAULT_LIMIT = 500;
-const DEFAULT_DATE = "2024-05-02T06:47:20.547Z";
+const DEFAULT_DATE = '2024-05-02T06:47:20.547Z';
 
 export const getCategoriesFromAPI = async (
   createdAfter: string = DEFAULT_DATE,
   limit = DEFAULT_LIMIT
 ): Promise<CategoryPagedQueryResponse> => {
   try {
-    const response = await apiRoot.categories().get({
-      queryArgs: {
-        limit,
-        where: `createdAt>"${createdAfter}"`
-      }
-    }).execute();
+    const response = await apiRoot
+      .categories()
+      .get({
+        queryArgs: {
+          limit,
+          where: `createdAt>"${createdAfter}"`,
+        },
+      })
+      .execute();
     return response.body;
   } catch (error) {
     throw new Error('Failed to fetch categories');
