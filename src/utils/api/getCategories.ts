ecdad0734e03c9c7d5024fa5c7e1e273c3e18936
Pavel Gordienko <guzmo@yandex.ru>
2024-06-03 15:22:18 +0300

    feat: get our category

--- src/utils/api/getCategories.ts
+++ src/utils/api/getCategories.ts
@@ -1,12 +1,22 @@
 import { CategoryPagedQueryResponse } from '@commercetools/platform-sdk';
 import { apiRoot } from './getProjectInfo';
 
-export const getCategoriesFromAPI =
-  async (): Promise<CategoryPagedQueryResponse> => {
-    try {
-      const response = await apiRoot.categories().get().execute();
-      return response.body;
-    } catch (error) {
-      throw new Error('Failed to fetch categories');
-    }
-  };
+const DEFAULT_LIMIT = 500;
+const DEFAULT_DATE = "2024-05-02T06:47:20.547Z";
+
+export const getCategoriesFromAPI = async (
+  createdAfter: string = DEFAULT_DATE,
+  limit = DEFAULT_LIMIT
+): Promise<CategoryPagedQueryResponse> => {
+  try {
+    const response = await apiRoot.categories().get({
+      queryArgs: {
+        limit,
+        where: `createdAt>"${createdAfter}"`
+      }
+    }).execute();
+    return response.body;
+  } catch (error) {
+    throw new Error('Failed to fetch categories');
+  }
+};
