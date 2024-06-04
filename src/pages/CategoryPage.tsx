09c7e29dacf4709f29c28e82ad7560693a091026
Pavel Gordienko <guzmo@yandex.ru>
2024-06-04 11:20:57 +0300

    feat: add category page

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,5 +1,15 @@
-const CategoryPage = () => {
-  return null;
+import { useState } from 'react';
+import { ProductInfo } from '../utils/Interfaces';
+import ProductGrid from '../components/ProductGrid';
+
+const CategoryPage: React.FC = () => {
+  const [products, setProducts] = useState<ProductInfo[]>([]);
+
+  return (
+    <div className="catalog">
+      <ProductGrid products={products} setProducts={setProducts} />
+    </div>
+  );
 };
 
 export default CategoryPage;
