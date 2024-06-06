65934866184554000f261630cae3ddc230a9ee74
Vladyslava <90470263+VlaRu@users.noreply.github.com>
2024-06-06 15:15:44 +0200

    fix: fix buggs with search api (#70)

---
+++ src/utils/api/getSearch.ts
@@ -0,0 +1,23 @@
+import { apiRoot } from './BuildClient';
+
+export const getSearchFromAPI = async (text: string, fuzzyLevelValue: number = 0) => {
+  try {
+    const response = await apiRoot
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          "text.en": text,
+          limit: 500,
+          offset: 0,
+          fuzzy: true,
+          fuzzyLevel: fuzzyLevelValue
+        },
+      })
+      .execute();
+      console.log(`respoooooonse ${response}`);
+  } catch (error) {
+    console.error('Error fetching displaytechnology:', error);
+    return [];
+  }
+};
