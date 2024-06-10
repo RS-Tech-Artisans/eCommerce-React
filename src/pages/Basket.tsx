c5ccb6f857a37282953321f33469d620e9528f8b
Pavel Gordienko <guzmo@yandex.ru>
2024-06-10 08:22:10 +0300

    feat: add empty-cart-message and link Catalog Page

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -1,3 +1,25 @@
-export default function Basket() {
-  return <h1>Basket</h1>;
-}
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Basket.css';
+
+const Basket: React.FC = () => {
+  const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+  const isEmptyCart = cart.length === 0;
+
+  return (
+    <div className="basket-container">
+      {isEmptyCart ? (
+        <div className="empty-cart-message">
+          <p>Your shopping cart is empty. Start shopping now!</p>
+          <p>
+            Go to <Link to="/catalog">Product Catalog</Link>
+          </p>
+        </div>
+      ) : (
+        <h1>Basket</h1>
+      )}
+    </div>
+  );
+};
+
+export default Basket;
