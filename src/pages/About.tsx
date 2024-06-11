31aa53b344e1d7267ee2e6688fd3531924ea76ca
Pavel Gordienko <guzmo@yandex.ru>
2024-06-11 03:30:21 +0300

    feat: add logo rs school in about page (#77)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,19 @@
-export default function About() {
-  return <h1>About Us</h1>;
-}
+import React from 'react';
+import './About.css';
+
+const About: React.FC = () => {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <a
+        href="https://rs.school/courses/javascript-ru"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="about-logo"></div>
+      </a>
+    </div>
+  );
+};
+
+export default About;
