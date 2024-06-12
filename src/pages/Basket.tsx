764d4c01100030f92a9e31f35af353396e91cd0a
Ranika23 <vanika90@yandex.ru>
2024-06-13 02:54:58 +0300

    feat: add function of check exist cart via API request

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Basket.css';
 import ClearCartButton from '../common/ClearCartButton';
+import { useSession } from '../utils/SessionContext';
+import { checkExistCart } from '../utils/api/checkExistCart';
 
 const Basket: React.FC = () => {
+  const { token } = useSession();
   const [cartExists, setCartExists] = useState<boolean>(false);
+  const [checkCartApiResult, setCheckCartApiResult] = useState<void>();
 
   useEffect(() => {
     const savedCart = JSON.parse(localStorage.getItem('cart') || '[]');
@@ -16,8 +20,21 @@ const Basket: React.FC = () => {
     setCartExists(false);
   };
 
+  useEffect(() => {
+    const checkCart = async () => {
+      try {
+        const response: void = await checkExistCart(token);
+        if (response !== undefined) setCheckCartApiResult(response);
+      } catch (error) {
+        console.error('Error checking exist cart:', error);
+      }
+    };
+
+    checkCart();
+  }, []);
+
   return (
-    <div className="basket-container">
+    <div className="basket-container">     
       {!cartExists ? (
         <div className="empty-cart-message">
           <p>Your shopping cart is empty. Start shopping now!</p>
@@ -28,6 +45,7 @@ const Basket: React.FC = () => {
       ) : (
         <>
           <h1>Basket</h1>
+          <div>CHECK EXIST CART:{JSON.stringify(checkCartApiResult)}</div>
           <div className="shopping-cart">
             <ClearCartButton onClearCart={clearCart} />
           </div>
