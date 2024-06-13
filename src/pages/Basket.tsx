9c5c4ef9dd01f3a0075467e73b2828ed401a39c4
VlaRu <dn210690nvs@gmail.com>
2024-06-13 15:03:00 +0200

    refactor: committed fail code

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Basket.css';
+import { getDiscountAPI } from '../utils/api/getDiscountApi';
+import { Cart } from '@commercetools/platform-sdk';
 
 const Basket: React.FC = () => {
   const cart = JSON.parse(localStorage.getItem('cart') || '[]');
   const isEmptyCart = cart.length === 0;
+
+  const [promoCode, setPromoCode] = useState('');
+
+  //здесь мне нужно будет для теста уже созданную Cart из реализации пункта 02
+  //const [cartData, setCartData] = useState<Cart | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // getDiscountAPI(promoCode, cartData);
+  };
 
   return (
     <div className="basket-container">
@@ -16,7 +28,21 @@ const Basket: React.FC = () => {
           </p>
         </div>
       ) : (
-        <h1>Basket</h1>
+        <div>
+          <h1>Basket</h1>
+          <h2>Promo is: QLED-TV</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="promocode">Promocode:</label>
+            <input
+              type="text"
+              id="promocode"
+              value={promoCode}
+              onChange={(e): void => setPromoCode(e.target.value)}
+            />
+            <input type="submit" value="Apply" />
+          </form>
+          <span>Price with discount: </span>
+        </div>
       )}
     </div>
   );
---
+++ src/utils/api/getDiscountApi.ts
@@ -0,0 +1,22 @@
+import { Cart } from "@commercetools/platform-sdk";
+import { apiRoot } from "./BuildClient";
+
+export const getDiscountAPI = async (promoCode: string, cartData: Cart) => {
+  try {
+    const response = await apiRoot
+      .me()
+      .carts()
+      .withId({ ID: cartData.id })
+      .post({
+        body: {
+          version: cartData.version,
+          actions: [{ action: 'addDiscountCode', code: promoCode }],
+        },
+      })
+      .execute();
+    return response.body
+  } catch (error) {
+    console.error(`discount ${promoCode} not founded`, error);
+    return [];
+  }
+};
