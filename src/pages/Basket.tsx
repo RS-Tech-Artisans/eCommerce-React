aa2b77afe77239e364b79cede705af3933079597
Pavel Gordienko <guzmo@yandex.ru>
2024-06-16 04:32:40 +0300

    fix: add error condition

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -315,7 +315,7 @@ const Basket: React.FC = () => {
               </Button>
               {showMessage.type && (
                 <div
-                  className={`toast ${showMessage.type === 'success' ? 'show' : ''}`}
+                  className={`toast ${showMessage.type === 'success' || showMessage.type === 'error' ? 'show' : ''}`}
                 >
                   {showMessage.text}
                 </div>
