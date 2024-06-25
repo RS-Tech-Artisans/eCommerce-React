83d258d36e9b2700625b692f422e1b1aefb6d9e7
Pavel Gordienko <guzmo@yandex.ru>
2024-06-26 02:13:49 +0300

    feat: add constants fix description

---
+++ src/utils/Constants.ts
@@ -0,0 +1,6 @@
+export const AUTH_HOST = 'https://auth.us-central1.gcp.commercetools.com';
+export const API_HOST = 'https://api.us-central1.gcp.commercetools.com';
+export const PROJECT_KEY = 'my-company';
+export const CLIENT_ID = 'RlxVza_Z9B7Fm83frzN4ks58';
+export const CLIENT_SECRET = 'A1PzY6KA6kCT0VwHhKzyQhoiToAqIWDa';
+export const SCOPES = ['manage_project:my-company'];
