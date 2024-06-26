3f551e333487ff7338cd1a6aa4b5c0c5c3d564ec
Pavel Gordienko <guzmo@yandex.ru>
2024-06-26 19:27:20 +0300

    feat: moved data

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,6 +1,6 @@
 export const AUTH_HOST = 'https://auth.us-central1.gcp.commercetools.com';
 export const API_HOST = 'https://api.us-central1.gcp.commercetools.com';
-export const PROJECT_KEY = 'my-company';
-export const CLIENT_ID = 'RlxVza_Z9B7Fm83frzN4ks58';
-export const CLIENT_SECRET = 'A1PzY6KA6kCT0VwHhKzyQhoiToAqIWDa';
-export const SCOPES = ['manage_project:my-company'];
+export const PROJECT_KEY = 'mymymy';
+export const CLIENT_ID = 'TLb92dPwr5ldn0QGFEh3_tE_';
+export const CLIENT_SECRET = '21gypTMv7HZUDAGQzlDoDJymW8oduNpP';
+export const SCOPES = ['manage_project:mymymy'];
